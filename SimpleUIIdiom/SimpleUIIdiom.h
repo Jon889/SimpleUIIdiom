2ea36aef1a91e91c6251b7fd698426f6b801212c
Jonathan <jon.bailey889@gmail.com>
2011-12-24 14:40:14 +0000

    Initial Commit

---
+++ SimpleUIIdiom/SimpleUIIdiom.h
@@ -0,0 +1,13 @@
+//
+//  SimpleUIIdiom.h
+//  SimpleUIIdiom
+//
+//  Created by Jonathan Bailey on 24/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleUIIdiom : NSObject
+
+@end
---
+++ SimpleUIIdiom/SimpleUIIdiom.m
@@ -0,0 +1,23 @@
+//
+//  SimpleUIIdiom.m
+//  SimpleUIIdiom
+//
+//  Created by Jonathan Bailey on 24/12/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SimpleUIIdiom.h"
+
+@implementation SimpleUIIdiom
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
